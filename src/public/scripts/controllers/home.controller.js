8de096448095abceaec142a43d5a1b7dae171795
Hunter Rancourt <skwid138@gmail.com>
2017-12-14 15:41:24 -0600

    initial commit, sourced in vendor files, bbasic index.html setup, basic client.js setup, basic home controller setup

---
+++ src/public/scripts/controllers/home.controller.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+
+/*
+This controller is for the home view.
+-  
+*/
+
+myApp.controller('HomeController', function (GithubService, MailService, $mdDialog) {
+    console.log('in HomeController');
+    const vm = this;
+
+
+
+    /************** $http **************/
+
+
+
+    /************** on page load **************/
+
+
+
+}); // end HomeController
