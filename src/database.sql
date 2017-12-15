f1cf7e20da32c5c177e2f0907e042079a7681a3e
Hunter Rancourt <skwid138@gmail.com>
2017-12-15 13:40:04 -0600

    sql script is working as expected

--- src/database.sql
+++ src/database.sql
@@ -3,14 +3,32 @@
 -- CREATE DATABASE mn-senate;
 
 
--- ** Drop Foreign Key Contraints
-
+-- ** Drop Foreign Key Contraints ** --
+ALTER TABLE IF EXISTS constituents
+    DROP CONSTRAINT IF EXISTS messages_fk;
+ALTER TABLE IF EXISTS senators
+    DROP CONSTRAINT IF EXISTS districts_fk;
 
 
 -- ** Drop Tables** --
-
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS districts;
+DROP TABLE IF EXISTS senators;
+DROP TABLE IF EXISTS constituents;
 
 -- ** Create Tables ** --
+
+-- messages
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    message TEXT
+);
+
+--districts
+CREATE TABLE districts (
+    id SERIAL PRIMARY KEY,
+    number INT
+);
 
 -- constituents
 CREATE TABLE constituents (
@@ -27,27 +45,29 @@ CREATE TABLE constituents (
     messages_id INT
 ); 
 
-CREATE TABLE messages {
-    id SERIAL PRIMARY KEY,
-    message TEXT
-};
-
-CREATE TABLE districts {
-    id SERIAL PRIMARY KEY,
-    number INT
-};
-
-CREATE TABLE senators {
+-- senators
+CREATE TABLE senators (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
-    district_id
-};
+    districts_id INT
+);
 
--- ** Add Foreign Key Constraints
 
--- constituents foreign keys
+-- ** Add Foreign Key Constraints ** --
+
+-- constituents foreign key
 ALTER TABLE constituents
     ADD CONSTRAINT messages_fk
         FOREIGN KEY (messages_id)
         REFERENCES messages (id)
+;
+
+-- senators foreign key
+ALTER TABLE senators
+    ADD CONSTRAINT districts_fk
+        FOREIGN KEY (districts_id)
+        REFERENCES districts (id)
+;
+
+-- ** Insert Mock Data ** --
