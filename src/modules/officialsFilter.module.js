94b2bb4ba1959fa904a5624af7788117e36c0fcd
Hunter Rancourt <skwid138@gmail.com>
2017-12-16 17:05:05 -0600

    adding comments for how to filter data before handing to client

--- src/modules/officialsFilter.module.js
+++ src/modules/officialsFilter.module.js
@@ -13,6 +13,11 @@ officials - an array of objects - this has a name property with social media and
 
 const filterSenators = (apiResponse) => {
 
+    // for office in apiResponse.data.offices
+    // office.officialIndicies[0] - this is the index of 
+    // the corresponding senator in apiResponse.data.officials
+
+
 }; // end filterSenators
 
 module.exports = filterSenators;
