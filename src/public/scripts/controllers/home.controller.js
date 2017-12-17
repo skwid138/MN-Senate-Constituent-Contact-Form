59ee8e2ca64e36cc3119722bac3779e9a2ed9373
Hunter Rancourt <skwid138@gmail.com>
2017-12-17 16:07:09 -0600

    added senator names and districts into drop down menus, built in logic for showign which one and resetting the page

--- src/public/scripts/controllers/home.controller.js
+++ src/public/scripts/controllers/home.controller.js
@@ -9,14 +9,41 @@ myApp.controller('HomeController', function ($http) {
     console.log('in HomeController');
     const vm = this;
 
+    // object to hold senators information
+    vm.senators = {};
+
+    // boolean for showing dropDown buttons
+    vm.dropDownButton = true;
+
+    // click event for showing search drop down
+    vm.showDropDown = (buttonId) => {
+        if (buttonId == 0) {
+            vm.showSearchName = true;
+        } else if (buttonId == 1) {
+            vm.showSearchDistrict = true;
+        } // end else if
+        // hide buttons
+        vm.dropDownButton = false;
+        // show reset/start over button
+        vm.reset = true;
+    }; // end showDropDown
+
+    // reset form and start from the beginning
+    vm.startOver = () => {
+        vm.dropDownButton = true;
+        vm.showSearchName = false;
+        vm.showSearchDistrict = false;
+        vm.reset = false;
+    }; // end startOver
 
 
     /************** $http **************/
 
-    // test get
-    vm.testAPI = () => {
+    // get senators information from Google's Civic API
+    vm.getSenators = () => {
         return $http.get('/senators').then((response) => {
             console.log('response.data', response.data);
+            vm.senators.list = response.data;
         }); // end return
     }; // end testAPI
 
@@ -24,6 +51,6 @@ myApp.controller('HomeController', function ($http) {
     /************** on page load **************/
 
     // see what data is returned
-    vm.testAPI();
+    vm.getSenators();
 
 }); // end HomeController
